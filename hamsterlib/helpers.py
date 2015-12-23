143377a5f3425192d893a82132a920b3a2648a7a
elbenfreund <github.10.elbenfreund@spamgourmet.org>
2015-12-23 17:45:44 +0100

    Added 'complete_timeframe' helper function.

--- hamsterlib/helpers.py
+++ hamsterlib/helpers.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import re
 import datetime
+from gettext import gettext as _
 
 
 TimeFrame = namedtuple('Timeframe', ('start_date', 'start_time',
@@ -54,4 +55,70 @@ def parse_time_info(time_info):
                 offset=None
             )
     return result
+
+
+def complete_timeframe(timeframe, day_start):
+    """Apply fallback strategy to incomplete timeframes."""
+
+    def complete_start_date(date):
+        if not date:
+            date = datetime.date.today()
+        else:
+            if not isinstance(date, datetime.date):
+                raise TypeError(_(
+                    "Expected datetime.date instance, got {type} instead.".format(
+                        type=type(date))
+                ))
+        return date
+
+    def complete_start_time(time, day_start):
+        if not time:
+            time = day_start
+        else:
+            if not isinstance(time, datetime.time):
+                raise TypeError(_(
+                    "Expected datetime.time instance, got {type} instead.".format(
+                        type=type(time))
+                ))
+        return time
+
+    def complete_end_date(date, start_date):
+        if not date:
+            date = start_date + datetime.timedelta(days=1)
+        else:
+            if not isinstance(date, datetime.date):
+                raise TypeError(_(
+                    "Expected datetime.date instance, got {type} instead.".format(
+                        type=type(date))
+                ))
+        return date
+
+    def complete_end_time(time, start_time):
+        if not time:
+            # create a pseudo datetime object so we can operate with timedelta
+            time = datetime.datetime.combine(datetime.date.today(),
+                start_time)
+            time = (time - datetime.timedelta(seconds=1)).time()
+        else:
+            if not isinstance(time, datetime.time):
+                raise TypeError(_(
+                    "Expected datetime.time instance, got {type} instead.".format(
+                        type=type(time))
+                ))
+        return time
+
+
+    if not timeframe.offset:
+        start = datetime.datetime.combine(
+            complete_start_date(timeframe.start_date),
+            complete_start_time(timeframe.start_time, day_start),
+        )
+    else:
+        start = datetime.datetime.now() - timeframe.offset
+
+    end = datetime.datetime.combine(
+        complete_end_date(timeframe.end_date, start.date()),
+        complete_end_time(timeframe.end_time, start.time())
+    )
+    return (start, end)
 
