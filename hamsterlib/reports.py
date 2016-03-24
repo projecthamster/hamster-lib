1c4e8d36b1f0059021cb2e6f54ba7fc2b0015b58
elbenfreund <github.10.elbenfreund@spamgourmet.org>
2016-03-24 16:52:42 +0100

    Reports: Refactored code, added tests.

--- hamsterlib/reports.py
+++ hamsterlib/reports.py
@@ -6,38 +6,92 @@ from gettext import gettext as _
 from collections import namedtuple
 
 
+"""
+Module to provide generic reporting capabilities for easy adaption by clients.
+
+The basic idea is to provide ``Writer`` classes that take care of the bulk of the setup
+upon instanciation so all the client needs to do is to call ``write_report`` with a list
+of ``FactTuples`` as arguments.
+"""
+
 FactTuple = namedtuple('FactTuple', ('start', 'end', 'activity', 'category',
     'description', 'duration'))
 
 
 class ReportWriter(object):
     def __init__(self, path, datetime_format="%Y-%m-%d %H:%M:%S"):
+        """
+        Initiate new instance and open an output file like object.
+
+        Note:
+            If you need added bells and wristels (like heading etc.) this would propably
+            the method to extend.
+
+        Args:
+            path: File like object to be opend. This is where all output will be directed to.
+            datetime_format (str): String specifying how datetime information is to be
+                rendered in the output.
+        """
         self.datetime_format = datetime_format
-        self.file = open(path, 'w')
+        self.file = open(path, 'w', encoding='utf-8')
 
     def write_report(self, facts):
-            for fact in facts:
-                if fact.category:
-                    category = fact.category.name
-                else:
-                    category = ''
+        """
+        Write facts to file output and make sure the file like object is closed at the end.
 
-                if fact.description:
-                    description = fact.description
-                else:
-                    description = ''
-                fact = FactTuple(
-                    start=fact.start.strftime(self.datetime_format),
-                    end=fact.end.strftime(self.datetime_format),
-                    activity=fact.activity.name,
-                    duration=fact.get_string_delta('%H:%M'),
-                    category=category,
-                    description=description,
-                )
-                self._write_fact(fact)
-            self._close()
+        Args:
+            facts (Iterable): Iterable of ``hamsterlib.Fact`` instances to be exported.
+
+        Returns:
+            None: If everything worked as expected.
+        """
+        for fact in facts:
+            self._write_fact(self._fact_to_tuple(fact))
+        self._close()
+
+    def _fact_to_tuple(self, fact):
+        """
+        Convert a ``Fact`` to its normalized tuple.
+
+        This is where all type conversion for ``Fact`` attributes to strings as well
+        as any normalization happens.
+
+        Args:
+            fact (hamsterlib.Fact): Fact to be converted.
+
+        Returns:
+            FactTuple: Tuple representing the original ``Fact``.
+        """
+        # Fields that may have ``None`` value will be represented by ''
+        if fact.category:
+            category = fact.category.name
+        else:
+            category = ''
+
+        description = fact.description or ''
+
+        return FactTuple(
+            start=fact.start.strftime(self.datetime_format),
+            end=fact.end.strftime(self.datetime_format),
+            activity=fact.activity.name,
+            duration=fact.get_string_delta('%H:%M'),
+            category=category,
+            description=description,
+        )
 
     def _write_fact(self, fact):
+        """
+        Represent one ``Fact`` in the output file.
+
+        What this means exactly depends on the format and kind of output.
+        At this point all type conversions and normalization have already been done.
+
+        Args:
+            fact (FactTuple): The individual fact to be written.
+
+        Returns:
+            None
+        """
         raise NotImplementedError
 
     def _close(self):
---
+++ tests/test_reports.py
@@ -0,0 +1,106 @@
+# -*- encoding: utf-8 -*-
+
+
+import pytest
+import os.path
+import csv
+
+from hamsterlib import reports
+
+
+@pytest.fixture
+def path(tmpdir):
+    return tmpdir.mkdir('reports').join('report.txt').strpath
+
+
+@pytest.fixture
+def report_writer(path):
+    return reports.ReportWriter(path)
+
+
+@pytest.fixture
+def tsv_writer(path):
+    return reports.TSVWriter(path)
+
+
+class TestReportWriter(object):
+    @pytest.mark.parametrize('datetime_format', [None, '%Y-%m-%d'])
+    def test_init_stores_datetime_format(self, path, datetime_format):
+        """Make sure that Writer initialization stores the ``datetime_format``."""
+        writer = reports.ReportWriter(path, datetime_format)
+        assert writer.datetime_format == datetime_format
+
+    def test_init_file_opened(self, path):
+        """Make sure a file like object is beeing opened."""
+        writer = reports.ReportWriter(path)
+        assert os.path.isfile(path)
+        assert writer.file.closed is False
+
+    def test__fact_to_tuple_no_category(self, report_writer, fact):
+        """Make sure that ``None`` category values translate to ``empty strings``."""
+        fact.activity.category = None
+        result = report_writer._fact_to_tuple(fact)
+        assert result.category == ''
+
+    def test__fact_to_tuple_with_category(self, report_writer, fact):
+        """Make sure that category references translate to their names."""
+        result = report_writer._fact_to_tuple(fact)
+        assert result.category == fact.category.name
+
+    def test_write_report_write_lines(self, mocker, report_writer, list_of_facts):
+        """Make sure that each ``Fact`` instances triggers a new line."""
+        number_of_facts = 10
+        facts = list_of_facts(number_of_facts)
+        report_writer._write_fact = mocker.MagicMock(return_value=None)
+        report_writer.write_report(facts)
+        assert report_writer._write_fact.call_count == number_of_facts
+
+    def test_write_report_file_closed(self, report_writer, list_of_facts):
+        """Make sure our output file is closed at the end."""
+        facts = list_of_facts(10)
+        with pytest.raises(NotImplementedError):
+            report_writer.write_report(facts)
+        assert report_writer.file.closed is False
+
+    def test__close(self, report_writer, path):
+        """Ensure that the the output gets closed."""
+        report_writer._close()
+        assert report_writer.file.closed
+
+
+class TestTSVWriter(object):
+    def test_init_csv_writer(self, tsv_writer):
+        """Make sure that initialition provides us with a ``csv.writer`` instance."""
+        assert tsv_writer.csv_writer
+        assert tsv_writer.csv_writer.dialect == csv.get_dialect('excel-tab')
+
+    def test_init_heading(self, path, tsv_writer):
+        """Make sure that initialition writes header as expected."""
+
+        expectation = (
+            ("start time"),
+            ("end time"),
+            ("activity"),
+            ("category"),
+            ("description"),
+            ("duration minutes"),
+        )
+
+        tsv_writer._close()
+        with open(path, 'r', encoding='utf-8') as fobj:
+            reader = csv.reader(fobj, dialect='excel-tab')
+            header = next(reader)
+
+        assert tuple(header) == expectation
+
+    def test__write_row(self, path, fact, tsv_writer):
+        fact_tuple = tsv_writer._fact_to_tuple(fact)
+        tsv_writer._write_fact(fact_tuple)
+        tsv_writer._close()
+        with open(path, 'r', encoding='utf-8') as fobj:
+            reader = csv.reader(fobj, dialect='excel-tab')
+            next(reader)
+            line = next(reader)
+        print(line)
+        assert tuple(line) == fact_tuple
+
