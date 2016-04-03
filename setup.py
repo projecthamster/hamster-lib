19dd21458042f00edce1a28d99588c5fb07082dc
elbenfreund <github.10.elbenfreund@spamgourmet.org>
2016-04-03 15:04:52 +0200

    Added SQLAlchemy to package requirements.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    'future',
+    'future', 'sqlalchemy',
 ]
 
 test_requirements = [
