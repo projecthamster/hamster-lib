90c3e9ddc493cf8e778d0c7540f47226c9818835
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2016-06-27 16:05:19 +0200

    Minor fix to docstring/license

--- hamster_lib/helpers.py
+++ hamster_lib/helpers.py
@@ -2,9 +2,9 @@
 
 # Copyright (C) 2015-2016 Eric Goller <eric.goller@ninjaduck.solutions>
 
-# This file is part of 'hamste-lib'.
+# This file is part of 'hamster-lib'.
 #
-# 'hamster_lib' is free software: you can redistribute it and/or modify
+# 'hamster-lib' is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
