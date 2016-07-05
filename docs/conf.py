b58bfc9ea2f0c7db3837ff058bebd00cbb1596e3
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2016-07-05 14:29:48 +0200

    Add 'docs' testenv to travis and tox (#165)
    We had to disable the linkchecker for our ``docs`` testenvironment.
    An issue related to this has been created as #172.
    
    Closes: #165

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,9 @@ extensions = [
     'alabaster',
 ]
 
+# Prevent non local immage warnings from showing
+suppress_warnings = ['image.nonlocal_uri']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -174,7 +177,7 @@ html_theme_path = [alabaster.get_path()]
 # here, relative to this directory. They are copied after the builtin
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
